8cb83fcef599d9cc1810d7d38185a61e12f3299a
Taha-codes <tahachadli9@gmail.com>
2025-02-21 10:22:06 +0100

    matrix multiplication and transposition

--- model.py
+++ model.py
@@ -85,7 +85,15 @@ class MultiHeadAttentionBlock(nn.Module):
         self.w_o = nn.Linear(d_model, d_model)
 
     def forward(self, q, k, v, mask):
-        query = self.w_q(q)
-        key = self.w_k(k)
-        value = self.w_v(v)
+        query   = self.w_q(q) #(Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key     = self.w_k(k)
+        value   = self.w_v(v)
+
+        #(Batch, seq_len, d_model) --> (Batch, seq_len, h, d_k) --> (Batch, h, seq_len, d_k)
+        query   = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key     = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value   = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
         
+
+
